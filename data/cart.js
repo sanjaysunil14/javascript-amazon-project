c2b2b99529b8b3cf9f943998f5174a36f9f84e3d
srudhidnpl <sanjaysunil145@gmail.com>
2025-07-13 10:25:42 +0530

    delivery option updated

--- data/cart.js
+++ data/cart.js
@@ -1,67 +1,54 @@
-export let cart=JSON.parse(localStorage.getItem('cart'));
+let cart = JSON.parse(localStorage.getItem('cart'));
 
-if (!cart){
-    cart=[{
-        productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity:2,
-        deliveryOptionId:'1'
-    },{
-        productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity:1,
-        deliveryOptionId:'2'
-    }];
+if (!cart) {
+  cart = [
+    {
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    },
+    {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
+    }
+  ];
+  saveToStorage();
 }
 
-function saveToStorage(){
-    localStorage.setItem('cart',JSON.stringify(cart));
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
 }
 
-export function addtocart(productId){
-    let matchingItem;
+export function getCart() {
+  return cart;
+}
 
-    cart.forEach((cartItem)=>{
-        if (productId===cartItem.productId){
-            matchingItem=cartItem;
-        }
-    });
+export function addToCart(productId) {
+  const matchingItem = cart.find(item => item.productId === productId);
 
-    if (matchingItem) {
-        matchingItem.quantity+=1;
-    }else{
-
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
     cart.push({
-        productId:productId,
-        quantity:1,
-        deliveryOptionsId:'1'
+      productId,
+      quantity: 1,
+      deliveryOptionId: '1'
     });
-    }
+  }
 
-    saveToStorage();
-    }
-
-export function removeFromCart(productId){
-    const newCart=[];
-    cart.forEach((cartItem)=>{
-        if (cartItem.productId !== productId){
-            newCart.push(cartItem);
-        }
-    });
-    cart=newCart;
-
-    saveToStorage();
+  saveToStorage();
 }
 
-export function updateDeliveryOption(productId,deliveryOptionId){
-    let matchingItem;
+export function removeFromCart(productId) {
+  cart = cart.filter(item => item.productId !== productId);
+  saveToStorage();
+}
 
-    cart.forEach((cartItem)=>{
-        if (productId===cartItem.productId){
-            matchingItem=cartItem;
-        }
-    });
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  const matchingItem = cart.find(item => item.productId === productId);
+  if (matchingItem) {
     matchingItem.deliveryOptionId = deliveryOptionId;
-
     saveToStorage();
-
+  }
 }
-    
